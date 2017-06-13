3a57d24132c9959e33df19e7cace4894192e631f
jonathanlee <jlee@serenetics.com>
2017-06-12 20:40:53 -0500

    mysin function

---
+++ library.c
@@ -0,0 +1,25 @@
+#include "library.h"
+
+#include <stdio.h>
+#include <math.h>
+#include <lua.h>
+
+static int l_sin (lua_State *L);
+
+int luaopen_simplefunc (lua_State *L) {
+
+    lua_pushcfunction(L, l_sin);
+    lua_setglobal(L, "mysin");
+
+    return 1;
+}
+
+static int l_sin (lua_State *L) {
+    double d = lua_tonumber(L, 1);  // get argument
+    // double d = luaL_checknumber(L, 1);
+
+    lua_pushnumber(L, sin(d));  // push result value
+
+    return 1;  /* number of results */
+}
+
